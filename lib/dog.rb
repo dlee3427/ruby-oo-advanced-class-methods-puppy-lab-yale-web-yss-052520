0e66cabaa4b2ca8466e0ef2dd4ac106ab4a2c09d
dlee3427 <david.lee.sl2634@yale.edu>
2020-05-27 20:17:58 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+require 'pry'
+class Dog 
+    def name 
+        @name 
+    end
+    
+    def name=(name)
+        @name = name
+    end 
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end 
+
+    def save 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end
+
+    def self.clear_all 
+        @@all.clear
+    end
+
+    def self.print_all
+        puts @@all.map { |puppy| puppy.name }
+    end 
+end 
